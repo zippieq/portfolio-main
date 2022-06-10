7b18579e783ccdcd312225bbedebc51999d0bf78
Hua Chi Quan <zippieq@gmail.com>
2022-06-10 07:51:53 +0700

    fix hp

--- static/js/home.js
+++ static/js/home.js
@@ -3,9 +3,9 @@ $(function() {
   $(".typed").typed({
     strings: [
       "stat rish.human<br/>" + 
-      "><span class='caret'>$</span> skills: android, hardware-software interaction, camera, computer vision<br/> ^100" +
-      "><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +
-      "><span class='caret'>$</span> hobbies: football, travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
+      "><span class='caret'>$</span> skills: c++, python, embedded software, data pipeline<br/> ^100" +
+      "><span class='caret'>$</span> job: data engineer <a href='https://www.fpt-software.com//'>FPT Software</a><br/> ^100" +
+      "><span class='caret'>$</span> hobbies: drawing, travel, <a href='http://www.zippieq.space/blog'>writing</a><br/> ^300" +
       "><span class='caret'>$</span> alias: crearo <br/>" +
       "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>"/*
       "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://zippieq.blogspot.com/'>blog</a><br/>"*/
