37c2141ecb371311cae75a48cbede8d4f31214c9
Hua Chi Quan <zippieq@gmail.com>
2022-06-10 08:09:50 +0700

    crearo

--- static/js/home.js
+++ static/js/home.js
@@ -6,9 +6,9 @@ $(function() {
       "><span class='caret'>$</span> skills: c++, python, embedded software, data pipeline<br/> ^100" +
       "><span class='caret'>$</span> job: data engineer <a href='https://www.fpt-software.com//'>FPT Software</a><br/> ^100" +
       "><span class='caret'>$</span> hobbies: drawing, travel, <a href='http://www.zippieq.space/blog'>writing</a><br/> ^300" +
-      "><span class='caret'>$</span> alias: crearo <br/>" +
+      "><span class='caret'>$</span> alias: zippieq <br/>" +
       "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>"/*
-      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/hua-chi-quan/'>linkedin</a> <a href='http://zippieq.blogspot.com/'>blog</a><br/>"*/
+      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/zippieq/'>github</a> <a href='http://in.linkedin.com/in/hua-chi-quan/'>linkedin</a> <a href='http://zippieq.blogspot.com/'>blog</a><br/>"*/
     ],
     showCursor: true,
     cursorChar: '_',
